2be3e1fa4f551f84586db1d35ab886df632c025a
Amit Kapadia <akapad@gmail.com>
2014-12-05 17:51:51 -0800

    expose bands in initi

---
+++ l8/__init__.py
@@ -0,0 +1,14 @@
+
+BANDS = {
+    "Coastal aerosol": 1,
+    "Blue": 2,
+    "Green": 3,
+    "Red": 4,
+    "NIR": 5,
+    "SWIR 1": 6,
+    "SWIR 2": 7,
+    "Panchromatic": 8,
+    "Cirrus": 9,
+    "TIRS 1": 10,
+    "TIRS 2": 11
+}
