08d698a91a9563c6f0a9aa35da59354608ecfe8e
Amit Kapadia <akapad@gmail.com>
2014-12-05 17:52:54 -0800

    remove bands dict

--- l8/spectrum.py
+++ l8/spectrum.py
@@ -8,22 +8,9 @@ import pyproj
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from l8 import BANDS
+
 sns.set()
-
-
-BANDS = {
-    "Coastal aerosol": 1,
-    "Blue": 2,
-    "Green": 3,
-    "Red": 4,
-    "NIR": 5,
-    "SWIR 1": 6,
-    "SWIR 2": 7,
-    "Panchromatic": 8,
-    "Cirrus": 9,
-    "TIRS 1": 10,
-    "TIRS 2": 11
-}
 
 
 def extract(scene_directory, longitude, latitude, bands=[]):
