71b015344d5ad608255f14c5a466a4d407b3e1db
Amit Kapadia <akapad@gmail.com>
2014-12-05 17:52:38 -0800

    cli for timeseries function

--- l8/scripts/cli.py
+++ l8/scripts/cli.py
@@ -1,6 +1,7 @@
 
 import click
 from l8 import spectrum as l8spectrum
+from l8 import timeseries as l8timeseries
 
 
 @click.group()
@@ -16,4 +17,13 @@ def spectrum(directory, longitude, latitude):
     l8spectrum.extract(directory, longitude, latitude)
 
 
+@click.command('timeseries')
+@click.argument('directories', nargs=-1)
+@click.option('--longitude')
+@click.option('--latitude')
+def timeseries(directories, longitude, latitude):
+    l8timeseries.extract(directories, longitude, latitude)
+
+
 l8.add_command(spectrum)
+l8.add_command(timeseries)
